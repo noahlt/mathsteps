0ff0b3df27c8ebcbed51a6e5372557c7ec6fb050
Noah Tye <hi@noahtye.com>
2018-05-11 14:38:45 -0700

    extract getQuadraticCoefficients and export it

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const ChangeTypes = require('./lib/ChangeTypes');
 const factor = require('./lib/factor');
 const simplifyExpression = require('./lib/simplifyExpression');
 const solveEquation = require('./lib/solveEquation');
+const getQuadraticCoefficients = require('./lib/factor/getQuadraticCoefficients');
 
 module.exports = {
   factor,
   simplifyExpression,
   solveEquation,
   ChangeTypes,
+  getQuadraticCoefficients,
 };
