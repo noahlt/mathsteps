c613c39b6dfef65e1706f46e88e2578879a0e66a
Noah Tye <hi@noahtye.com>
2018-06-27 16:00:43 -0700

    export NodeStatus and EquationStatus

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const simplifyExpression = require('./lib/simplifyExpression');
 const solveEquation = require('./lib/solveEquation');
 const getQuadraticCoefficients = require('./lib/factor/getQuadraticCoefficients');
 const removeUnnecessaryParens = require('./lib/util/removeUnnecessaryParens');
+const NodeStatus = require('./lib/node/Status');
+const EquationStatus = require('./lib/equation/Status');
 
 module.exports = {
   factor,
@@ -12,4 +14,6 @@ module.exports = {
   ChangeTypes,
   getQuadraticCoefficients,
   removeUnnecessaryParens,
+  NodeStatus,
+  EquationStatus,
 };
